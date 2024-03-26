1fa8f594869e9cc4bd5b6061730b4f0e651bec23
Rizal Kalam <89380544+rizalkalam@users.noreply.github.com>
2024-03-26 08:35:41 +0700

    Update katalog.js

--- katalog.js
+++ katalog.js
@@ -1,261 +1,261 @@
 const data_katalog = [
-    {
-      link: "Detail-Produk/katalog2.html",
-      img: "./Assets/Katalog/katalog2.jpg",
-      name: "Paper Bag Haji dan Umroh",
-      category: "Paper Bag",
-      size: "Ukuran : 27cm X 32cm X 7cm",
-      price: "Rp 17.000/lusin"
-    },
-    {
-      link: "Detail-Produk/katalog3.html",
-      img: "./Assets/Katalog/katalog3.jpg",
-      name: "Paper Bag Terimakasih",
-      category: "Paper Bag",
-      size: "Ukuran : 17cm X 23cm X 5cm",
-      price: "Rp 275/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog4.html",
-      img: "./Assets/Katalog/katalog4.jpg",
-      name: "Tas Paper Bag Brownies",
-      category: "Paper Bag",
-      size: "Ukuran : 35cm X 14cm X 18cm",
-      price: "Rp 15.000/lusin"
-    },
-    {
-      link: "Detail-Produk/katalog1.html",
-      img: "./Assets/Katalog/katalog1.jpg",
-      name: "Tas Spunbond Motif Pita",
-      category: "Tas Spunbond",
-      size: "Ukuran : 30cm X 38cm X 10cm",
-      price: "Rp 4.500/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog6.html",
-      img: "./Assets/Katalog/katalog6.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 30cm X 35cm",
-      price: "Rp 4.500/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog8.html",
-      img: "./Assets/Katalog/katalog8.jpg",
-      name: "Tas SPunbond Motif",
-      category: "Tas Spunbond",
-      size: "Ukuran : 20cm X 25cm X 5cm",
-      price: "Rp 4.700/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog17.html",
-      img: "./Assets/Katalog/katalog17.jpg",
-      name: "Tas Spunbond Mika",
-      category: "Tas Spunbond",
-      size: "Ukuran : 25cm X 30cm X 24cm",
-      price: "Rp 5.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog7.html",
-      img: "./Assets/Katalog/katalog7.jpg",
-      name: "Tas Spunbond Mika",
-      category: "Tas Spunbond",
-      size: "Ukuran : 25cm X 30cm X 14cm",
-      price: "Rp 4.700/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog5.html",
-      img: "./Assets/Katalog/katalog5.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 23cm X 23cm X 30cm",
-      price: "Rp 4.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog9.html",
-      img: "./Assets/Katalog/katalog9.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 23cm X 23cm X 30cm",
-      price: "Rp 4.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog10.html",
-      img: "./Assets/Katalog/katalog10.jpg",
-      name: "Tas Spunbond Motif",
-      category: "Tas Spunbond",
-      size: "Ukuran : 23cm X 30cm X 12cm",
-      price: "Rp 3.250/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog20.html",
-      img: "./Assets/Katalog/katalog20.jpg",
-      name: "Tas Spunbond Motif",
-      category: "Tas Spunbond",
-      size: "Ukuran : 23cm X 30cm X 12cm",
-      price: "Rp 3.250/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog11.html",
-      img: "./Assets/Katalog/katalog11.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 23cm X 30cm X 12cm",
-      price: "Rp 4.500/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog12.html",
-      img: "./Assets/Katalog/katalog12.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 25cm X 30cm X 14cm",
-      price: "Rp 5.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog13.html",
-      img: "./Assets/Katalog/kataloog13.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 20cm X 20cm X 14cm",
-      price: "Rp 4.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog14.html",
-      img: "./Assets/Katalog/katalog14.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 25cm X 35cm X 14cm",
-      price: "Rp 5.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog15.html",
-      img: "./Assets/Katalog/katalog15.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 30cm X 30cm X 14cm",
-      price: "Rp 5.500/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog18.html",
-      img: "./Assets/Katalog/katalog18.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 23cm X 30cm X 23m",
-      price: "Rp 6.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog19.html",
-      img: "./Assets/Katalog/katalog19.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 23cm X 30cm X 23m",
-      price: "Rp 6.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog27.html",
-      img: "./Assets/Katalog/katalog27.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 34cm X 28cm X 14cm",
-      price: "Rp 5.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog21.html",
-      img: "./Assets/Katalog/katalog21.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 30cm X 38cm X 10cm",
-      price: "Rp 4.500/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog22.html",
-      img: "./Assets/Katalog/katalog22.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 30cm X 38cm X 12cm",
-      price: "Rp 4.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog24.html",
-      img: "./Assets/Katalog/katalog24.jpg",
-      name: "Tas Totebag",
-      category: "Totebag",
-      size: "Ukuran : 25cm X 30cm",
-      price: "Rp 7.200/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog25.html",
-      img: "./Assets/Katalog/katalog25.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 35cm X 40cm X 7,5cm",
-      price: "Rp 7.500/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog23.html",
-      img: "./Assets/Katalog/katalog23.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 35cm X 45cm X 14cm",
-      price: "Rp 11.500/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog28.html",
-      img: "./Assets/Katalog/katalog28.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 35cm X 45cm X 14cm",
-      price: "Rp 11.500/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog29.html",
-      img: "./Assets/Katalog/katalog29.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 35cm X 45cm X 14cm",
-      price: "Rp 11.500/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog26.html",
-      img: "./Assets/Katalog/katalog26.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond",
-      size: "Ukuran : 35cm X 45cm X 14cm",
-      price: "Rp 7.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog30.html",
-      img: "./Assets/Katalog/katalog30.jpg",
-      name: "Tas Spunbond Serut",
-      category: "Tas Spunbond Serut",
-      size: "Ukuran : 21cm X 30cm X 21cm",
-      price: "Rp 5.000/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog31.html",
-      img: "./Assets/Katalog/katalog31.jpg",
-      name: "Tas Spunbond Serut",
-      category: "Tas Spunbond Serut",
-      size: "Ukuran : 21cm X 30cm X 21cm",
-      price: "Rp 4.500/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog16.html",
-      img: "./Assets/Katalog/katalog16.jpg",
-      name: "Tas Spunbond",
-      category: "Tas Spunbond Serut",
-      size: "Ukuran : 30cm X 30cm X 7cm",
-      price: "Rp 6.500/pcs"
-    },
-    {
-      link: "Detail-Produk/katalog32.html",
-      img: "./Assets/Katalog/katalog32.jpg",
-      name: "Tas Spunbond Toples",
-      category: "Tas Spunbond Serut",
-      size: "Ukuran : Toples 1/2 kg",
-      price: "Rp 3.500/pcs"
-    },
-  ];
+  {
+    link: "PaperBag_HajidanUmroh",
+    img: "./Assets/Katalog/katalog2.jpg",
+    name: "Paper Bag Haji dan Umroh",
+    category: "Paper Bag",
+    size: "Ukuran : 27cm X 32cm X 7cm",
+    price: "Rp 17.000/lusin"
+  },
+  {
+    link: "PaperBag_Terimakasih",
+    img: "./Assets/Katalog/katalog3.jpg",
+    name: "Paper Bag Terimakasih",
+    category: "Paper Bag",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "TasPaperBag_Brownies",
+    img: "./Assets/Katalog/katalog4.jpg",
+    name: "Tas Paper Bag Brownies",
+    category: "Paper Bag",
+    size: "Ukuran : 20cm X 20cm",
+    price: "Rp 2.000/pcs"
+  },
+  {
+    link: "TasSpunbond_MotifPita",
+    img: "./Assets/Katalog/katalog1.jpg",
+    name: "Tas Spunbond Motif Pita",
+    category: "Tas Spunbond",
+    size: "Ukuran : 30cm X 38cm X 10cm",
+    price: "Rp 4.500/pcs"
+  },
+  {
+    link: "Tas_Spunbond1",
+    img: "./Assets/Katalog/katalog6.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 30cm X 35cm",
+    price: "Rp 4.500/pcs"
+  },
+  {
+    link: "TasSPunbond_Motif1",
+    img: "./Assets/Katalog/katalog8.jpg",
+    name: "Tas SPunbond Motif",
+    category: "Tas Spunbond",
+    size: "Ukuran : 20cm X 25cm X 5cm",
+    price: "Rp 4.700/pcs"
+  },
+  {
+    link: "TasSpunbond_Mika1",
+    img: "./Assets/Katalog/katalog17.jpg",
+    name: "Tas Spunbond Mika",
+    category: "Tas Spunbond",
+    size: "Ukuran : 25cm X 30cm X 24cm",
+    price: "Rp 5.000/pcs"
+  },
+  {
+    link: "TasSpunbond_Mika2",
+    img: "./Assets/Katalog/katalog7.jpg",
+    name: "Tas Spunbond Mika",
+    category: "Tas Spunbond",
+    size: "Ukuran : 25cm X 30cm X 14cm",
+    price: "Rp 4.700/pcs"
+  },
+  {
+    link: "TasSpunbond1",
+    img: "./Assets/Katalog/katalog5.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 23cm X 23cm X 30cm",
+    price: "Rp 4.000/pcs"
+  },
+  {
+    link: "TasSpunbond2",
+    img: "./Assets/Katalog/katalog9.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 23cm X 23cm X 30cm",
+    price: "Rp 4.000/pcs"
+  },
+  {
+    link: "TasSpunbond_Motif2",
+    img: "./Assets/Katalog/katalog10.jpg",
+    name: "Tas Spunbond Motif",
+    category: "Tas Spunbond",
+    size: "Ukuran : 23cm X 30cm X 12cm",
+    price: "Rp 3.250/pcs"
+  },
+  {
+    link: "TasSpunbond_Motif3",
+    img: "./Assets/Katalog/katalog20.jpg",
+    name: "Tas Spunbond Motif",
+    category: "Tas Spunbond",
+    size: "Ukuran : 23cm X 30cm X 12cm",
+    price: "Rp 3.250/pcs"
+  },
+  {
+    link: "TasSpunbond3",
+    img: "./Assets/Katalog/katalog11.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 23cm X 30cm X 12cm",
+    price: "Rp 4.500/pcs"
+  },
+  {
+    link: "TasSpunbond4",
+    img: "./Assets/Katalog/katalog12.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 25cm X 30cm X 14cm",
+    price: "Rp 5.000/pcs"
+  },
+  {
+    link: "TasSpunbond5",
+    img: "./Assets/Katalog/kataloog13.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 20cm X 20cm X 14cm",
+    price: "Rp 4.000/pcs"
+  },
+  {
+    link: "TasSpunbond6",
+    img: "./Assets/Katalog/katalog14.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 25cm X 35cm X 14cm",
+    price: "Rp 5.000/pcs"
+  },
+  {
+    link: "TasSpunbond7",
+    img: "./Assets/Katalog/katalog15.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 30cm X 30cm X 14cm",
+    price: "Rp 5.500/pcs"
+  },
+  {
+    link: "TasSpunbond8",
+    img: "./Assets/Katalog/katalog18.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 23cm X 30cm X 23m",
+    price: "Rp 6.000/pcs"
+  },
+  {
+    link: "TasSpunbond9",
+    img: "./Assets/Katalog/katalog19.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 23cm X 30cm X 23m",
+    price: "Rp 6.000/pcs"
+  },
+  {
+    link: "TasSpunbond10",
+    img: "./Assets/Katalog/katalog27.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 34cm X 28cm X 14cm",
+    price: "Rp 5.000/pcs"
+  },
+  {
+    link: "TasSpunbond11",
+    img: "./Assets/Katalog/katalog21.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 30cm X 38cm X 10cm",
+    price: "Rp 4.500/pcs"
+  },
+  {
+    link: "TasSpunbond12",
+    img: "./Assets/Katalog/katalog22.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 30cm X 38cm X 12cm",
+    price: "Rp 4.000/pcs"
+  },
+  {
+    link: "TasTotebag",
+    img: "./Assets/Katalog/katalog24.jpg",
+    name: "Tas Totebag",
+    category: "Totebag",
+    size: "Ukuran : 25cm X 30cm",
+    price: "Rp 7.200/pcs"
+  },
+  {
+    link: "TasSpunbond13",
+    img: "./Assets/Katalog/katalog25.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 35cm X 40cm X 7,5cm",
+    price: "Rp 7.500/pcs"
+  },
+  {
+    link: "TasSpunbond14",
+    img: "./Assets/Katalog/katalog23.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 35cm X 45cm X 14cm",
+    price: "Rp 11.500/pcs"
+  },
+  {
+    link: "TasSpunbond15",
+    img: "./Assets/Katalog/katalog28.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 35cm X 45cm X 14cm",
+    price: "Rp 11.500/pcs"
+  },
+  {
+    link: "TasSpunbond16",
+    img: "./Assets/Katalog/katalog29.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 35cm X 45cm X 14cm",
+    price: "Rp 11.500/pcs"
+  },
+  {
+    link: "TasSpunbond17",
+    img: "./Assets/Katalog/katalog26.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond",
+    size: "Ukuran : 35cm X 45cm X 14cm",
+    price: "Rp 7.000/pcs"
+  },
+  {
+    link: "TasSpunbond_Serut1",
+    img: "./Assets/Katalog/katalog30.jpg",
+    name: "Tas Spunbond Serut",
+    category: "Tas Spunbond Serut",
+    size: "Ukuran : 21cm X 30cm X 21cm",
+    price: "Rp 5.000/pcs"
+  },
+  {
+    link: "TasSpunbond_Serut2",
+    img: "./Assets/Katalog/katalog31.jpg",
+    name: "Tas Spunbond Serut",
+    category: "Tas Spunbond Serut",
+    size: "Ukuran : 21cm X 30cm X 21cm",
+    price: "Rp 4.500/pcs"
+  },
+  {
+    link: "TasSpunbond18",
+    img: "./Assets/Katalog/katalog16.jpg",
+    name: "Tas Spunbond",
+    category: "Tas Spunbond Serut",
+    size: "Ukuran : 30cm X 30cm X 7cm",
+    price: "Rp 6.500/pcs"
+  },
+  {
+    link: "TasSpunbond_Toples",
+    img: "./Assets/Katalog/katalog32.jpg",
+    name: "Tas Spunbond Toples",
+    category: "Tas Spunbond Serut",
+    size: "Ukuran : Toples 1/2 kg",
+    price: "Rp 3.500/pcs"
+  },
+];
 
   const filterSelect = document.getElementById("filter-katalog");
   const katalogDataContainer = document.getElementById("katalog-list");
